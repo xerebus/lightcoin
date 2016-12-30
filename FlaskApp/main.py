ff422512019cdea2817265cc64a3a4238c90416e
xerebus <aritrabiswas@gmail.com>
2016-12-29 20:56:43 -0600

    created frontpage in HTML

--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -3,13 +3,13 @@
 #
 # Main Flask routines for lightcoin.
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "test"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
